7aeb9dace4ae0fa2e7ace67607ce861a4486b152
nayan <nayanrahul.jnv@gmail.com>
2019-02-11 10:19:15 +0530

    few fixes

--- public/js/app.js
+++ public/js/app.js
@@ -49278,8 +49278,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /srv/http/chat/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /srv/http/chat/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/chat_App/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/chat_App/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
